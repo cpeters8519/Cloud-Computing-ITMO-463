95c0669871c7309424a8b855a1e193c5dfced14e
cpeters8519 <cpeters8519@gmail.com>
2026-03-29 15:48:21 -0500

    update main

--- Module 2 - Terraforming/main.tf
+++ Module 2 - Terraforming/main.tf
@@ -1,29 +1,27 @@
 ##############################################################################
-# Create and RDS Mysql database - get dbname, username, and password using the
-# ${var} function
-# Create Output blocks for the db-name and db-address
-# Note this isn't the most secure -- we will fix it in the next modules
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance
+# Create an RDS MySQL database
 ##############################################################################
 resource "aws_db_instance" "project_db" {
-  allocated_storage    = 10
-  db_name              = var.dbname
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = var.uname
-  password             = var.pass
-  skip_final_snapshot  = true
+  allocated_storage   = 10
+  db_name             = var.dbname
+  engine              = "mysql"
+  engine_version      = "8.0"
+  instance_class      = "db.t3.micro"
+  username            = var.uname
+  password            = var.pass
+  skip_final_snapshot = true
+
+  tags = {
+    Name        = var.tag-name
+    Environment = "Dev"
+  }
 }
 
 ##############################################################################
-# Create 2 S3 buckets - pull their names from the terraform.tfvars using the
-# ${var} function
-# Use output blocks to display the URL of the buckets
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+# Create 2 S3 buckets
 ##############################################################################
 resource "aws_s3_bucket" "raw_bucket" {
-  bucket = 
+  bucket = var.raw-bucket
 
   tags = {
     Name        = var.tag-name
@@ -32,7 +30,7 @@ resource "aws_s3_bucket" "raw_bucket" {
 }
 
 resource "aws_s3_bucket" "finished_bucket" {
-  bucket = 
+  bucket = var.finished-bucket
 
   tags = {
     Name        = var.tag-name
@@ -47,25 +45,21 @@ output "raw_url" {
 
 ##############################################################################
 # Create an SNS Topic
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sns_topic
 ##############################################################################
 resource "aws_sns_topic" "user_updates" {
-  name = 
+  name = var.sns-topic
 
   tags = {
-    Name        = 
+    Name        = var.tag-name
     Environment = "project"
   }
 }
 
-
 ##############################################################################
-# Create an SQS Message Queue and print out the URL in an output block
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sqs_queue
+# Create an SQS Queue
 ##############################################################################
 resource "aws_sqs_queue" "terraform_queue" {
-  name = 
-
+  name = var.sqs
 
   tags = {
     Name        = var.tag-name
@@ -75,31 +69,27 @@ resource "aws_sqs_queue" "terraform_queue" {
 
 ##############################################################################
 # Create launch template
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/launch_template
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template
 ##############################################################################
 resource "aws_launch_template" "lt" {
-   image_id = 
-   instance_type = 
-   key_name = 
-   vpc_security_group_ids = []
+  image_id             = var.imageid
+  instance_type        = var.instance-type
+  key_name             = var.key-name
+  vpc_security_group_ids = [var.vpc_security_group_ids]
 
-     tags = {
-       Name = var.tag-name
-     }
-    user_data = filebase64("./install-env.sh")
+  tags = {
+    Name = var.tag-name
+  }
+
+  user_data = filebase64("./install-env.sh")
 }
-  
+
 ##############################################################################
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpcs
+# Get default VPC and subnets
 ##############################################################################
 data "aws_vpc" "main" {
   default = true
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/subnets
-# Gather a list of all subnets per AZ per your region
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
@@ -107,100 +97,83 @@ data "aws_subnets" "public" {
   }
 }
 
-# Print out to screen a list of subnets
 output "list-of-subnets" {
   description = "List of subnets"
-  value = data.aws_subnets.public.ids
+  value       = data.aws_subnets.public.ids
 }
 
-# Get all AZs in a VPC
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# Print out a list of all AZs in your VPC per region
 output "list-of-azs" {
   description = "List of AZs"
-  value = data.aws_availability_zones.available.names
+  value       = data.aws_availability_zones.available.names
 }
 
 ##############################################################################
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
+# Create Load Balancer
 ##############################################################################
 resource "aws_lb" "lb" {
-  name               = 
-  internal           = 
-  load_balancer_type = 
-  security_groups    = []
+  name               = var.elb-name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.vpc_security_group_ids]
   subnets            = data.aws_subnets.public.ids
 
   enable_deletion_protection = false
 
   tags = {
-    Name = var.tag-name
+    Name        = var.tag-name
     Environment = "project"
   }
 }
 
-# Print the ELB URL to the CLI screen
 output "url" {
   value = aws_lb.lb.dns_name
 }
 
 ##############################################################################
-# Create autoscaling group
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group
+# Create Auto Scaling Group
 ##############################################################################
 resource "aws_autoscaling_group" "asg" {
-  name                      = 
-  max_size                  = 
-  min_size                  = 
+  name                      = var.asg-name
+  max_size                  = var.max
+  min_size                  = var.min
+  desired_capacity          = var.desired
   health_check_grace_period = 300
   health_check_type         = "EC2"
-  desired_capacity          = 
   force_delete              = true
-  target_group_arns         = []
-  availability_zones = 
+  target_group_arns         = [aws_lb_target_group.alb-lb-tg.arn]
+  availability_zones        = data.aws_availability_zones.available.names
 
   launch_template {
     id = aws_launch_template.lt.id
-  }  
-  
+  }
 }
 
 ##############################################################################
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_attachment
-##############################################################################
-# Create a new ALB Target Group attachment
-resource "aws_autoscaling_attachment" "asg-attach" {
-  # Wait for lb to be running before attaching the ASG
-  depends_on = [ aws_lb.lb ]
-  autoscaling_group_name = 
-  lb_target_group_arn = 
-
-}
-##############################################################################
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
+# Create ALB Target Group
 ##############################################################################
 resource "aws_lb_target_group" "alb-lb-tg" {
-  # Depends on - wait for LB to exist
-  depends_on = [ aws_lb.lb ]
-  name = 
+  depends_on  = [aws_lb.lb]
+  name        = var.tg-name
   target_type = "instance"
-  port = 
-  protocol = "HTTP"
-  vpc_id = 
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.main.id
 }
+
 ##############################################################################
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
+# Create ALB Listener
 ##############################################################################
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = 
-  port = "80"
-  protocol = "HTTP"
+  load_balancer_arn = aws_lb.lb.arn
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type = "forward"
-    target_group_arn = 
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-lb-tg.arn
   }
 }
