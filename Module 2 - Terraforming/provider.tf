f7817840f4c6df774ac5036734679e7ab155d3c4
cpeters8519 <cpeters8519@gmail.com>
2026-03-29 15:47:33 -0500

    update provider.tf

--- Module 2 - Terraforming/provider.tf
+++ Module 2 - Terraforming/provider.tf
@@ -9,7 +9,6 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  # Change this region to match your default region
-  region                   = "us-east-2"
-  shared_credentials_files = ["/home/vagrant/.aws/credentials"]
+  region  = "us-east-2"
+  profile = "cpeters8519-nonroot"
 }
